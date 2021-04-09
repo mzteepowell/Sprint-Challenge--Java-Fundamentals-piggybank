ded4ab54cc51bad9d13a74c20cbc8ba64f79fcc6
Tarah <tarah.agbokhana@gmail.com>
2021-04-09 11:44:53 -0400

    MVP

--- src/main/java/com/lambdaschool/piggybank/controllers/PiggyBankController.java
+++ src/main/java/com/lambdaschool/piggybank/controllers/PiggyBankController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -26,6 +28,28 @@ public class PiggyBankController {
     
     double total = 0.0;
     
+    for (PiggyBank e : piggyBankList) {
+      total += e.getQuantity() * e.getValue();
+      if (e.getQuantity() <= 1) {
+        System.out.print(e.getQuantity() + " " +  e.getName() + "\n");
+      }
+      else {
+        System.out.print(e.getQuantity() + " " + e.getNameplural() + "\n");
+      }
+    }
+    System.out.println("The piggy bank holds: " + total);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+  
+  //STRETCH
+  //http://localhost:2019/money/{amount}
+  @PutMapping(value = "/money{amount}", produces = "application/json")
+  public ResponseEntity<?> updateValues(@PathVariable double amount) {
+    List<PiggyBank> piggyBankList = new ArrayList<>();
+    piggyBankRepository.findAll().iterator().forEachRemaining(e -> piggyBankList.add(e));
+  
+    double total = 0.0;
+  
     for (PiggyBank e : piggyBankList) {
       total += e.getQuantity() * e.getValue();
       if (e.getQuantity() <= 1) {
